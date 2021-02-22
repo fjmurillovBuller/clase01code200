539259cf9957fff8bc783b0435dcec9720d10295
francisco murilloV <fjmurillov3743@gmail.com>
2021-02-21 20:34:28 -0600

    enviamos los archivos al area local

---
+++ operacionMat.py
@@ -0,0 +1,23 @@
+localidades ={
+    30000: 'Nicaragua',
+    30100: 'Managua',
+    30101: 'Ciudad Sandino',
+    30102: 'El Crucero',
+    30103: 'Tipitapa',
+    30200: 'Masaya',
+    30201: 'Niquinomo',
+    30202: 'Nindiri',
+    30203: 'Catarina',
+    30300: 'Granada',
+    30301: 'La calzada',
+    30400: 'Boaco',
+    30401: 'Teustepe',
+    30402: 'San Lorenzo',
+    30403: 'La Cruz'
+}
+
+print(localidades[30200])
+
+codigo_pais = round(30402, -2)
+print(codigo_pais)
+print(localidades[codigo_pais])
